766d0220e02d886dca607d2453a39dc5d2de7982
AmIEviil <lokiinn@gmail.com>
2021-09-03 13:01:21 -0400

    confirmar

--- src/app/pages/ejemplo1/ejemplo1.page.ts
+++ src/app/pages/ejemplo1/ejemplo1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-ejemplo1',
   templateUrl: './ejemplo1.page.html',
@@ -8,7 +8,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Ejemplo1Page implements OnInit {
 
-  constructor(private alertController: AlertController) { }
+  constructor(private router:Router, private alertController: AlertController) { }
 
   ngOnInit() {
     this.listar();
@@ -30,11 +30,11 @@ export class Ejemplo1Page implements OnInit {
     console.log('onDidDismiss resolved with role', role);
   }
   //Mensaje de alerta para eliminar
-  async AlertaConfirmarEliminar(rut:String) {
+  async AlertaConfirmarEliminar(rut: String) {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Eliminar!',
-      message: 'Desea Eliminar el rut <strong>'+ rut + '</strong> ?!',
+      message: 'Desea Eliminar el rut <strong>' + rut + '</strong> ?!',
       buttons: [
         {
           text: 'Cancelar',
@@ -74,18 +74,57 @@ export class Ejemplo1Page implements OnInit {
 
 
   //Metodos
-  eliminar(rut:String){
-    alert('Selecciono Eliminar: '+ rut);
-  }
-  actualizar(rut:String){
-    alert('Selecciono Actualizar: '+ rut);
-  }
-  listar(){
+  eliminar(rut: String) {
+    alert('Selecciono Eliminar: ' + rut);
     var datos = localStorage.getItem('misdatos');
-    
-    datos = datos.replace('[','');
-    datos = datos.replace(']','');
+
+    datos = datos.replace('[', '');
+    datos = datos.replace(']', '');
     datos = datos.split('},{').join('};{');
-    alert(datos);
+    //alert(datos);
+    var per;
+    var lista_temporal = new Array();
+    var arreglo_tempo = datos.split(";");
+    for (let index = 0; index < arreglo_tempo.length; index++) {
+      var registro = arreglo_tempo[index];
+      var lapersona = JSON.parse(registro);
+      per = {
+        rut: lapersona.rut,
+        nombre: lapersona.nombre,
+        edad: lapersona.edad
+      };
+      if(lapersona.rut != rut){
+        lista_temporal.push(per);  
+      }
+    }
+    this.lista_personas = lista_temporal;
+    localStorage.setItem('misdatos',JSON.stringify(lista_temporal));
   }
+
+  actualizar(rut: String) {
+    alert('Selecciono Actualizar: ' + rut);
+    this.router.navigate(['/ejemplo2',rut]);
+  }
+  listar() {
+    var datos = localStorage.getItem('misdatos');
+
+    datos = datos.replace('[', '');
+    datos = datos.replace(']', '');
+    datos = datos.split('},{').join('};{');
+    //alert(datos);
+    var per;
+    var lista_temporal = new Array();
+    var arreglo_tempo = datos.split(";");
+    for (let index = 0; index < arreglo_tempo.length; index++) {
+      var registro = arreglo_tempo[index];
+      var lapersona = JSON.parse(registro);
+      per = {
+        rut: lapersona.rut,
+        nombre: lapersona.nombre
+      };
+      lista_temporal.push(per);
+    }
+    this.lista_personas = lista_temporal;
+  }
+
 }
--- src/app/pages/ejemplo2/ejemplo2.page.ts
+++ src/app/pages/ejemplo2/ejemplo2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-ejemplo2',
   templateUrl: './ejemplo2.page.html',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ejemplo2Page implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    var valor = this.activatedRoute.snapshot.paramMap.get('valor');
+    //alert('Recupero parametro: ' + valor);
+    this.buscar(valor);
   }
+  persona = new FormGroup({
+    elRut: new FormControl(''),
+    elNombreCompleto: new FormControl(''),
+    elEdad: new FormControl('')
+  });
+  perso:any={
+    rut:''
+  };
 
+  buscar(rut: String) {
+    var datos = localStorage.getItem('misdatos');
+    datos = datos.replace('[', '');
+    datos = datos.replace(']', '');
+    datos = datos.split('},{').join('};{');
+    var arreglo_tempo = datos.split(";");
+    for (let index = 0; index < arreglo_tempo.length; index++) {
+      var registro = arreglo_tempo[index];
+      var lapersona = JSON.parse(registro);
+      if (lapersona.rut == rut) {
+        this.perso={
+          rut: lapersona.rut,
+          //nombre:lapersona.nombre,
+          //edad:lapersona.edad
+        };
+        this.persona.controls.elNombreCompleto.setValue(lapersona.nombre);
+      }
+    }
+
+  }
 }
